0cad9140f380e48751ef9d370c9765a37957c4e5
ramenbytes <36470445+ramenbytes@users.noreply.github.com>
2020-04-22 21:19:34 -0700

    Add comments import phconvert

--- gui.py
+++ gui.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+# For metadata file
 import yaml
+# For gui
 from tkinter import *
 from tkinter import ttk
+# For conversion library
+import phconvert as ph
+
+# Here's a thought: lets focus on getting a nice gui for editing the YAML
+# metadata. Link to the data groups:
+# https://photon-hdf5.readthedocs.io/en/latest/phdata.html
+#
+# Would want ways to selectively show groups, possibly more granular options too
+# like only supplied options, mandatory, optional, so on. I think different
+# setups have different requirements too.
