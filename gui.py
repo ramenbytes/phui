4730ad51e2bd6c3ebd265065584b91b0b2dab348
ramenbytes <36470445+ramenbytes@users.noreply.github.com>
2020-06-01 19:53:26 -0700

    Move most notes into org file
    The source file was starting to get cluttered.

--- gui.py
+++ gui.py
@@ -7,43 +7,8 @@ import yaml
 from pathlib import Path
 import matplotlib.pyplot as plt
 import phconvert as phc
-
 # debugging
 import pdb
-
-# Here's a thought: lets focus on getting a nice gui for editing the YAML
-# metadata. Link to the data groups:
-# https://photon-hdf5.readthedocs.io/en/latest/phdata.html
-#
-# Would want ways to selectively show groups, possibly more granular options too
-# like only supplied options, mandatory, optional, so on. I think different
-# setups have different requirements too.
-# def run(width='300', hight='200'):
-#     root = Tk()
-#     root.geometry(width + 'x' + hight)
-#     root.mainloop()
-
-# possible folding frame implementation starting point:
-# https://stackoverflow.com/questions/13141259/expandable-and-contracting-frame-in-tkinter
-# What is the license on StackOverflow postings?
-# Creative Commons type: https://stackoverflow.com/help/licensing
-#
-# Creating your own widgets:
-# https://mail.python.org/pipermail/tkinter-discuss/2008-August/001602.html
-# https://stackoverflow.com/questions/30489308/creating-a-custom-widget-in-tkinter
-# https://effbot.org/zone/tkinter3000-wck.htm
-# Widget creation kits:
-# https://effbot.org/zone/wck-1.htm
-# https://pypi.org/project/tkinter3000/
-# http://pmw.sourceforge.net/
-# https://sourceforge.net/projects/pmw/
-
-# On second thought, I think it may make more sense to develop the logic and
-# then form the gui around that. This is opposed to the gui-first attitude I've
-# had so far. If I could somehow have convenient way of dealing with the idea of
-# commands as opposed to raw events, that seems like it would be lovely. From
-# here on out, probably better to focus on core conversion logic and bootstrap
-# from there. As much as things can be bootstrapped in python...
 
 # Current goal: Point and shoot file conversion. i.e, convert(file) -> converted file
 # Current state: ?
