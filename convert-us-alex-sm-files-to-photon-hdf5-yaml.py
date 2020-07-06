3f9dcd4e1c72f7491eeab258b305c188063ce5e1
ramenbytes <36470445+ramenbytes@users.noreply.github.com>
2020-07-05 18:07:07 -0700

    Don't save hdf5 files with yml suffix
    Accidentally saved converted files with yml suffix, stomping on input file names.

--- convert-us-alex-sm-files-to-photon-hdf5-yaml.py
+++ convert-us-alex-sm-files-to-photon-hdf5-yaml.py
@@ -184,7 +184,7 @@ def load_and_poke(file, yml_file=False):
 
 def convert(file, yaml_file=False):
     data = load_and_poke(file, yaml_file)
-    phc.hdf5.save_photon_hdf5(data, h5_fname=Path(file).with_suffix('.yml'), overwrite=True, close=True)
+    phc.hdf5.save_photon_hdf5(data, h5_fname=Path(file).with_suffix('.hdf5'), overwrite=True, close=True)
     return
 
 phc.plotter.alternation_hist(data)
