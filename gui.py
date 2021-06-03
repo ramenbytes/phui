ed7099a9ed0f9d4aca0d429b216193918de89fee
ramenbytes <36470445+ramenbytes@users.noreply.github.com>
2021-06-02 19:39:05 -0700

    Start work on tkinter-based gui again
    Add comments detailing what the tkinter stuff does.

--- gui.py
+++ gui.py
@@ -7,3 +7,13 @@ import yaml
 from pathlib import Path
 # debugging
 import pdb
+
+
+def run(width='300', hight='200'):
+    # Creates the top-level widget and tcl process. We see a window pop up
+    # because of the creation of the top-level widget.
+    root = Tk()
+    # Sets the dimensions of our new window
+    root.geometry(width + 'x' + hight)
+    # Starts the event loop for the associated tcl process. This is a blocking call!
+    root.mainloop()
