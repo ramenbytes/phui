3095fb6bc30f5ee26d2ed17328d6414064f1d3f1
ramenbytes <36470445+ramenbytes@users.noreply.github.com>
2021-06-06 21:09:38 -0700

    Some basic tkinter playing around

--- gui.py
+++ gui.py
@@ -8,7 +8,6 @@ from pathlib import Path
 # debugging
 import pdb
 
-
 def run(width='300', hight='200'):
     # Creates the top-level widget and tcl process. We see a window pop up
     # because of the creation of the top-level widget.
@@ -17,3 +16,14 @@ def run(width='300', hight='200'):
     root.geometry(width + 'x' + hight)
     # Starts the event loop for the associated tcl process. This is a blocking call!
     root.mainloop()
+
+def root_with_geometry(width='300',height='200'):
+    root = Tk()
+    root.geometry(str(width) + 'x' + str(height))
+    return root
+
+# for some reason, stumpwm does not respect the resize when I eval the whole
+# buffer. Individually eval'ing each statement does the trick though. ???
+root = Tk()
+root.title('PHUI')
+root.geometry('300x200')
