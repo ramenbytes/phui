7676d2d10e79121564d1689688729d4628158e65
ramenbytes <36470445+ramenbytes@users.noreply.github.com>
2021-06-16 21:21:58 -0700

    Make target label into an entry

--- gui.py
+++ gui.py
@@ -50,9 +50,9 @@ target_frame.grid(column=0,row=0,sticky=(N, S, E, W))
 # button for conversion
 chosenfile = StringVar()
 chosenfile.set("<choose a target>")
-targetlabel = Label(target_frame, textvariable=chosenfile)
+targetentry = Entry(target_frame, textvariable=chosenfile)
 # the padding is to provide space between the label and the following button
-targetlabel.grid(column=0,padx=10)
+targetentry.grid(column=0,sticky=(W,E))
 
 def make_callback(finder,stringvar):
     '''Returns a closure over finder and stringvar. finder must be a function that
