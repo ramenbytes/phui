7973c6ebd5b9b3ae08bc2f543c396d09f163051c
ramenbytes <36470445+ramenbytes@users.noreply.github.com>
2021-12-20 14:12:07 -0800

    Add some print statements, which should show up in the command window
    This should help me see when the script is exiting.

--- gui.py
+++ gui.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 # For gui
+print("starting imports!")
+
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 from tkinter import messagebox
 
 import os
+print("importing yaml!")
 import yaml
 from pathlib import Path
 # debugging
 import pdb
 
+print("importing unified_conversion!")
 # unified conversion
 import unified_conversion as uc
 
+print("done importing!")
 def run(width='300', hight='200'):
     # Creates the top-level widget and tcl process. We see a window pop up
     # because of the creation of the top-level widget.
