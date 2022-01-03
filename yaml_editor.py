8b06563a27ccb177c1cb04d786324bf897502ec6
ramenbytes <36470445+ramenbytes@users.noreply.github.com>
2022-01-03 15:02:56 -0800

    Start working on displaying the specs to the users as a tree

--- yaml_editor.py
+++ yaml_editor.py
@@ -4,3 +4,14 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 from tkinter import messagebox
+
+from phconvert.metadata import official_fields_specs
+from phconvert.v04.metadata import official_fields_specs as v04_official_fields_specs
+
+# ok, how do I show users all the options? Also, how to do I automate the
+# translation of the specs to a tree, for later use? What I'd like to be able to
+# do is to more or less just say
+#
+# create_entry(dict, '<flattened_path_from_specs', description_list)
+#
+# to create an entry in the (possibly nested) dictionar(y/ies)
