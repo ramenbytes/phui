e08fc31e63e0115d394890bf5c2e384adca73724
ramenbytes <36470445+ramenbytes@users.noreply.github.com>
2022-01-03 14:47:15 -0800

    Start working on the metadata editor

---
+++ yaml_editor.py
@@ -0,0 +1,6 @@
+'''This module provides the editor for our restricted subset of yaml metadata'''
+
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter import messagebox
